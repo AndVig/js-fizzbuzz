899511bc7274676fcf07785f92f38cf6bdebe9a5
AndVig <andrea.vigna2@gmail.com>
2024-04-30 18:33:25 +0200

    add if cases

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,21 @@
 const element= document.getElementById('area');
 for(let i=1; i<=100; i++){
     const square= document.createElement('div')
-    square.className='blue square justify-content-center align-items-center m-3';
+    square.className='d-flex blue square justify-content-center align-items-center m-3';
     square.innerHTML=i;
     element.append(square);
+
+    if(i%3==0 && i%5==0){
+        square.className='d-flex red square justify-content-center align-items-center m-3';
+        square.innerHTML='fizzbuzz';
+    }
+    else if(i%3==0){
+        square.className='d-flex green square justify-content-center align-items-center m-3';
+        square.innerHTML='fizz';
+    }
+    else if(i%5==0){
+        square.className='d-flex yellow square justify-content-center align-items-center m-3';
+        square.innerHTML='buzz';
+    }
+    
 }
